817bb5a2618094f60c3a566b46a393421ff492ba
George Marriott <george@tutora.co.uk>
2024-10-22 19:24:43 +0100

    twinkl task cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header/Header";
 import { Body } from "./components/Body/Body";
 import "./assets/styles/app.scss";
 
+//define app with components
 const App = () => {
   return (
     <div>
